ca13add72d4712c637b54984e9d1e68c43624ac6
hakimex <adi.tiar56@gmail.com>
2026-04-22 11:33:15 +0700

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,10 +5,10 @@ require('dotenv').config();
 const sslConfig = process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false;
 
 const pool = mysql.createPool({
-  host:             process.env.DB_HOST     || 'localhost',
-  port:             parseInt(process.env.DB_PORT || '3306'),
-  user:             process.env.DB_USER     || 'root',
-  password:         process.env.DB_PASSWORD || '',
+  host:             process.env.DB_HOST     || 'jogja-furniture-db-adi-db97.d.aivencloud.com',
+  port:             parseInt(process.env.DB_PORT || '19730'),
+  user:             process.env.DB_USER     || 'avnadmin',
+  password:         process.env.DB_PASSWORD || 'AVNS_lTWcKifVEm7xHG8MZNq',
   database:         process.env.DB_NAME     || 'jogja_furniture',
   ssl:              sslConfig,
   waitForConnections: true,
